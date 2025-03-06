e928f1498e6be8243cc5e6c8d3a2458a66d4a737
Youngjae <2015fromjeki@gmail.com>
2025-03-06 20:36:34 +1100

    Finalised the Repository

--- lib/models/repository.dart
+++ lib/models/repository.dart
@@ -1,6 +1,7 @@
 import 'package:travel_app/models/item_list.dart';
 
 class Repository {
+  // Coffee List
   List<Itemlist> coffeeList = [
     Itemlist(
       backgroundImage: "../images/coffees/coffee0.jpg",
@@ -12,7 +13,7 @@ class Repository {
       isCart: false,
     ),
     Itemlist(
-      backgroundImage: "../images/coffees/coffee1.jpg",
+      backgroundImage: "../images/coffees/coffee1.png",
       title: "Cafe Serenade",
       location: "Melbourne",
       price: 8,
@@ -31,7 +32,7 @@ class Repository {
     ),
     Itemlist(
       backgroundImage: "../images/coffees/coffee3.jpg",
-      title: "The Roasted Bean",
+      title: "Morning Brew",
       location: "Perth",
       price: 7,
       review: 4,
@@ -39,4 +40,156 @@ class Repository {
       isCart: false,
     ),
   ];
+
+  // Ice Cream List
+  List<Itemlist> iceCreamList = [
+    Itemlist(
+      backgroundImage: "../images/icecream/icecream1.jpg",
+      title: "Icecream Frozen Delight",
+      location: "Sydney",
+      price: 5,
+      review: 4,
+      category: "ice cream",
+      isCart: false,
+    ),
+    Itemlist(
+      backgroundImage: "../images/icecream/icecream2.jpg",
+      title: "Icecream Choco Swirl",
+      location: "Melbourne",
+      price: 6,
+      review: 5,
+      category: "ice cream",
+      isCart: false,
+    ),
+    Itemlist(
+      backgroundImage: "../images/icecream/icecream3.jpg",
+      title: "Icecream Berry Bliss",
+      location: "Sydney",
+      price: 4,
+      review: 3,
+      category: "ice cream",
+      isCart: false,
+    ),
+    Itemlist(
+      backgroundImage: "../images/icecream/icecream4.jpg",
+      title: "Icecream Vanilla Dream",
+      location: "Melbourne",
+      price: 5,
+      review: 5,
+      category: "ice cream",
+      isCart: false,
+    ),
+  ];
+
+  // Chocolate List
+  List<Itemlist> chocolateList = [
+    Itemlist(
+      backgroundImage: "../images/chocolate/chocolate1.jpg",
+      title: "Dark Temptation",
+      location: "Sydney",
+      price: 3,
+      review: 5,
+      category: "chocolate",
+      isCart: false,
+    ),
+    Itemlist(
+      backgroundImage: "../images/chocolate/chocolate2.jpg",
+      title: "Milk Chocolate Dream",
+      location: "Melbourne",
+      price: 4,
+      review: 4,
+      category: "chocolate",
+      isCart: false,
+    ),
+    Itemlist(
+      backgroundImage: "../images/chocolate/chocolate3.jpg",
+      title: "Chocolate Cravings",
+      location: "Sydney",
+      price: 5,
+      review: 5,
+      category: "chocolate",
+      isCart: false,
+    ),
+    Itemlist(
+      backgroundImage: "../images/chocolate/chocolate4.jpg",
+      title: "Caramel Chocolate Delight",
+      location: "Melbourne",
+      price: 6,
+      review: 4,
+      category: "chocolate",
+      isCart: false,
+    ),
+  ];
+
+  // Cake List
+  List<Itemlist> cakeList = [
+    Itemlist(
+      backgroundImage: "../images/cakes/cake0.jpg",
+      title: "Velvet Indulgence",
+      location: "Sydney",
+      price: 15,
+      review: 4,
+      category: "cake",
+      isCart: false,
+    ),
+    Itemlist(
+      backgroundImage: "../images/cakes/cake1.jpg",
+      title: "Chocolate Fantasy",
+      location: "Melbourne",
+      price: 18,
+      review: 5,
+      category: "cake",
+      isCart: false,
+    ),
+    Itemlist(
+      backgroundImage: "../images/cakes/cake2.jpg",
+      title: "Lemon Cheesecake",
+      location: "Sydney",
+      price: 14,
+      review: 4,
+      category: "cake",
+      isCart: false,
+    ),
+    Itemlist(
+      backgroundImage: "../images/cakes/cake3.jpg",
+      title: "Strawberry Shortcake",
+      location: "Melbourne",
+      price: 16,
+      review: 5,
+      category: "cake",
+      isCart: false,
+    ),
+  ];
+  // 장바구니
+  List<Itemlist> itemCart = [];
+  // 장바구니 method + 추가
+  void addItem(Itemlist list) {
+    itemCart.add(list);
+  }
+
+  // 장바구니 method + 제거
+  void deleteItem(Itemlist list) {
+    itemCart.remove(list);
+  }
+
+  // 리스트 종류
+  // 커피샵 리스트
+  List<Itemlist> getCoffeeLists() {
+    return coffeeList;
+  }
+
+  // 케이크 리스트
+  List<Itemlist> getCakeList() {
+    return cakeList;
+  }
+
+  // 초콜릿샵 리스트
+  List<Itemlist> getChocolateList() {
+    return chocolateList;
+  }
+
+  // 아이스크림 리스트
+  List<Itemlist> getIcemcreamList() {
+    return iceCreamList;
+  }
 }
