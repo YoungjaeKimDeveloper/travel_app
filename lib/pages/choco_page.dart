69cc79cade92aefe243000c88e0d4f9c8c60e040
Youngjae <2015fromjeki@gmail.com>
2025-03-07 13:20:15 +1100

    Created choco_page

--- lib/pages/choco_page.dart
+++ lib/pages/choco_page.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:travel_app/components/category_tile.dart';
+import 'package:travel_app/models/repository.dart';
 
 class ChocoPage extends StatelessWidget {
   const ChocoPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("This is the Choco Page")));
+    // Get the Data from somewhere
+    return Consumer<Repository>(
+      builder: (context, value, child) {
+        return GridView.builder(
+          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            childAspectRatio: 0.8,
+          ),
+          itemCount: value.getChocolateList().length,
+          itemBuilder: (BuildContext context, int index) {
+            return CategoryTile(itemlist: value.getChocolateList()[index]);
+          },
+        );
+      },
+    );
   }
 }
